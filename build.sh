e83d30f2291798952802afd916e83e725d131312
pkgdemon <pkgdemonteam@gmail.com>
2024-06-23 18:14:23 -0400

    Fix GH detection

--- build.sh
+++ build.sh
@@ -8,13 +8,26 @@ CPUS=$(nproc)
 export GNUSTEP_INSTALLATION_DOMAIN=NETWORK
 . /Developer/Makefiles/GNUstep.sh
 
-if [ -f "/__w/developer/developer/system_amd64.zip" ]; then
-  export SRC="/__w/developer/developer//"
+# Initialize WORKDIR to empty (if not already set)
+export WORKDIR=""
+
+# Detect whether or not GitHub actions is being used
+if [ -d "/__w/developer/developer/" ]; then
+  echo "GH actions AMD64 runner detected"
+  export WORKDIR="/__w/developer/developer/"
 fi
 
-if [ -f "/home/runner/work/developer/developer/system_arm64.zip" ]; then
-  export SRC="/home/runner/work/developer/developer/"  
+if [ -d "/home/runner/work/developer/developer/" ]; then
+  echo "GH actions ARM64 runner detected"
+  export WORKDIR="/home/runner/work/developer/developer/"
 fi
 
-cd ${SRC}/apps-gorm && gmake -j"${CPUS}" || exit 1 && gmake install
-cd ${SRC}/apps-projectcenter && gmake -j"${CPUS}" || exit 1 || exit 1 && gmake install
+if [ -z "$WORKDIR" ]; then
+  echo "WORKDIR is empty, setting it to current working directory"
+  WORKDIR="$PWD"
+fi
+
+echo "WORKDIR is set to: $WORKDIR"
+
+cd ${WORKDIR}/apps-gorm && gmake -j"${CPUS}" || exit 1 && gmake install
+cd ${WORKDIR}/apps-projectcenter && gmake -j"${CPUS}" || exit 1 || exit 1 && gmake install
